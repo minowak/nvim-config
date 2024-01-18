c0f06738627c700f6f83cb0d20c5a30fe1cd84e1
Michal Nowak <michal@streetbeat.com>
2024-01-18 17:57:46 +0100

    Replaced old config

--- lua/plugins/4-dashboard.lua
+++ lua/plugins/4-dashboard.lua
@@ -27,13 +27,27 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New file", ":e <BAR> startinsert <CR>"),
-      dashboard.button("f", "  > Find file", ":cd $HOME/Projects| Telescope find_files<CR>"),
-      dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("c", "  > Config", ":e ~/.config/nvim/init.lua<CR> | :Neotree toggle<CR>"),
-      dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+      dashboard.button("e", "  New file", ":e <BAR> startinsert <CR>"),
+      dashboard.button("f", "  Find file", ":cd $HOME/Projects| Telescope find_files<CR>"),
+      dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("c", "  Config", ":e ~/.config/nvim/init.lua<CR> | :Neotree toggle<CR>"),
+      dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
     }
 
     alpha.setup(dashboard.opts)
+    vim.api.nvim_create_autocmd("User", {
+      callback = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime * 100) / 100
+        dashboard.section.footer.val = "󱐌 Lazy-loaded "
+            .. stats.loaded
+            .. "/"
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
   end,
 }
--- nvim_old/init.lua
+++
@@ -1,7 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("snippets.react")
-
-vim.cmd.colorscheme("tokyonight")
--- vim.cmd.colorscheme("dracula")
-vim.diagnostic.config({ float = { border = "rounded" } })
