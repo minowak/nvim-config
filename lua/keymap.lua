4ad3443604cbbf37a87664d36b2bc24e59444012
Michal Nowak <michal@streetbeat.com>
2024-01-21 00:08:07 +0100

    Fixed macros, custom bufferline pin

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -12,11 +12,11 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
--- Resize window using <opt> arrow keys
-map("n", "<A-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<A-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<A-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<A-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+-- Resize window using <opt> arrow keys (dont work in iterm)
+map("n", "<M-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<M-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<M-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<M-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move Lines
 map("n", "<M-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
--- lua/plugins/1-editor.lua
+++ lua/plugins/1-editor.lua
@@ -133,6 +133,15 @@ return {
           },
           globalstatus = true,
         },
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            }
+          },
+        },
       })
     end,
   },
