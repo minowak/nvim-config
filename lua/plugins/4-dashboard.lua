72a9ffd375d6a5578e18861f42fb1fe9e00b7176
Michal Nowak <michal@streetbeat.com>
2024-01-25 13:02:06 +0100

    Dashboard config fix + tint

--- lua/plugins/4-dashboard.lua
+++ lua/plugins/4-dashboard.lua
@@ -30,7 +30,7 @@ return {
       dashboard.button("e", "  New file", ":e <BAR> startinsert <CR>"),
       dashboard.button("f", "  Find file", ":cd $HOME/Projects| Telescope find_files<CR>"),
       dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("c", "  Config", ":e ~/.config/nvim/init.lua | :Neotree toggle<CR>"),
+      dashboard.button("c", "  Config", ":e ~/.config/nvim/init.lua<CR> | :Neotree toggle<CR>"),
       dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
     }
 
