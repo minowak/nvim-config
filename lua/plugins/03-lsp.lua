4ed7e51b5951ef2e9b0837cf6e006114f60cce12
Michal Nowak <michal@streetbeat.com>
2024-02-05 19:12:16 +0100

    Neotree LSP plugin for renaming files

--- lua/plugins/03-lsp.lua
+++ lua/plugins/03-lsp.lua
@@ -204,5 +204,15 @@ return {
     config = function()
       require("tailwindcss-colors").setup()
     end
-  }
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
